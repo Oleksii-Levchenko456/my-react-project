562c334c52eebc56f1fe3262b12ef596ad8e08c8
Oleksii Levchenko <neonator3000@gmail.com>
2025-09-24 14:00:52 +0300

    stady-tasks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,25 @@
-// src/App.tsx
-
-import { useState } from "react";
-
-interface Values {
-  x: number;
-  y: number;
-}
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [values, setValues] = useState<Values>({ x: 0, y: 0 });
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
 
-  const updateX = () => {
-    setValues({
-      ...values,
-      x: values.x + 1,
-    });
-  };
+  useEffect(() => {
+    console.log("First updated:", first);
+  }, [first]);
 
-  const updateY = () => {
-    setValues({
-      ...values,
-      y: values.y + 1,
-    });
-  };
+  useEffect(() => {
+    console.log("Second updated:", second);
+  }, [second]);
+
+  useEffect(() => {
+    console.log("First or second updated:", first + second);
+  }, [first, second]);
 
   return (
-    <div>
-      <p>x: {values.x}, y: {values.y}</p>
-      <button onClick={updateX}>Update x</button>
-      <button onClick={updateY}>Update y</button>
-    </div>
+    <>
+      <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+      <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 // src/main.tsx
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
-// Нормалізація стилів
-import "modern-normalize";
-// Глобальні стилі (додатково)
-import "./global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
